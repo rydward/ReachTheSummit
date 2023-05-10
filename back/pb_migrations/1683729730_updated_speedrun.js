9f3f572af0001ac8badfcb81404c41f5b3ec03e5
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-05-10 16:44:33 +0200

    Modif bdd + création flutter + test API

---
+++ back/pb_migrations/1683729730_updated_speedrun.js
@@ -0,0 +1,23 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("18qzyy9m3j7uwrh")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+  collection.createRule = ""
+  collection.updateRule = ""
+  collection.deleteRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("18qzyy9m3j7uwrh")
+
+  collection.listRule = null
+  collection.viewRule = null
+  collection.createRule = null
+  collection.updateRule = null
+  collection.deleteRule = null
+
+  return dao.saveCollection(collection)
+})
